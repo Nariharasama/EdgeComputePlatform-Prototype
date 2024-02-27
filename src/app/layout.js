97a1d1c31707baec6ecb6278b621a3fcb0353b54
HU SHENGYUAN <hushengyuan030831@gmail.com>
2024-02-27 22:43:32 +0800

    Add speedtest

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,5 +1,6 @@
 import { AppRouterCacheProvider } from "@mui/material-nextjs/v13-appRouter";
 import { StyledEngineProvider } from "@mui/material/styles";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 import "./globals.css";
 import ThemeContextProvider from "./theme";
 export const metadata = {
@@ -10,6 +11,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body>
+        <SpeedInsights />
         <StyledEngineProvider injectFirst>
           <AppRouterCacheProvider options={{ enableCssLayer: true }}>
             <ThemeContextProvider>
