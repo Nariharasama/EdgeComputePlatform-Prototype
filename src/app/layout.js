489d6810cb47816a6ca7f9b4976e5ff48368b089
HU SHENGYUAN <hushengyuan030831@gmail.com>
2024-02-27 23:04:18 +0800

    Support mobile device

--- src/app/layout.js
+++ src/app/layout.js
@@ -5,6 +5,9 @@ import "./globals.css";
 import ThemeContextProvider from "./theme";
 export const metadata = {
   title: "Prototype",
+  viewport: "width=720px,initial-scale=1",
+  formatDetection: "telephone=no,email=no,address=no",
+  charset: "UTF-8",
 };
 
 export default function RootLayout({ children }) {
